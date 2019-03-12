252fab9d7e992b8f5b380c41cd1eb33c50fec0eb
fanzhuyifan <fanzhuyifan@126.com>
2019-03-11 21:41:02 -0700

    Fix path in fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,5 @@
  source /usr/local/share/autojump/autojump.fish
  set fish_prompt_pwd_dir_length 3
 
- set PATH $PATH /usr/local/Cellar/python/3.7.0/bin
  # Use vi key binding
  fish_vi_key_bindings
