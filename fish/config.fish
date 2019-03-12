d6479ab251b5234e0b59bf045ae3ef779bf37dc4
fanzhuyifan <fanzhuyifan@126.com>
2019-03-11 20:53:07 -0700

    configure overwrites previous files; include fish configurations

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+ [ -f /usr/local/share/autojump/autojump.fish ] 
+ source /usr/local/share/autojump/autojump.fish
+ set fish_prompt_pwd_dir_length 3
+
+ set PATH $PATH /usr/local/Cellar/python/3.7.0/bin
+ # Use vi key binding
+ fish_vi_key_bindings
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,3 @@
+function fish_prompt
+  echo (whoami) (prompt_pwd) '> '
+end
